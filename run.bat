c761586c718b4e50e3a385d4a4e465c0f7cc5f6e
WizSam10 <85336448+CodingSam95@users.noreply.github.com>
2026-08-04 18:38:18 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not "%1"=="am_minimized" (
-    start "" /min "%~f0" am_minimized
-    exit /b
-)
-
-start "" "http://localhost:5500"
-"%~dp0caddy.exe" file-server --listen :5500 --root .
